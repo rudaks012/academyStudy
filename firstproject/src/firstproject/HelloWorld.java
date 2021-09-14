aa95b4ac7c6772fe465ae5372b8a0072de023919
admin <admin@YD02-16>
2021-09-14 11:16:59 +0900

    first

---
+++ firstproject/src/firstproject/HelloWorld.java
@@ -0,0 +1,11 @@
+package firstproject;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Hello,World!!!");
+		
+	}
+}
