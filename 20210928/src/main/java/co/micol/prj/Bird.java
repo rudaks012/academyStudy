956f2ae494fb18a03db2cfea58c9bbd6f1f6d77e
admin <admin@YD02-16>
2021-09-28 13:49:40 +0900

    20210928

---
+++ 20210928/src/main/java/co/micol/prj/Bird.java
@@ -0,0 +1,10 @@
+package co.micol.prj;
+
+public class Bird extends Animal {
+
+	@Override
+	public void run() {
+		System.out.println("새는 날아다닌다.");
+	}
+
+}
---
+++ 20210928/src/main/java/co/micol/prj/friend/Friend.java
@@ -0,0 +1,9 @@
+package co.micol.prj.friend;
+
+public abstract class Friend {
+	public String name;
+	public String tel;
+	public String address;
+
+	public abstract void friendPrint();
+}
