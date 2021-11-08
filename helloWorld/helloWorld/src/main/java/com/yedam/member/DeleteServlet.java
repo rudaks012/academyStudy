643471ac8ec3bf030c2ccf75cb7b3cdb7b0ebad9
rudak <rudak@DESKTOP-DLE9NFL>
2021-11-08 23:28:39 +0900

    1106

---
+++ helloWorld/helloWorld/src/main/java/com/yedam/member/DeleteServlet.java
@@ -0,0 +1,31 @@
+package com.yedam.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DeleteServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/json;charset=UTF-8");
+		
+		String title = request.getParameter("title");
+		MemDAO dao = new MemDAO();
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+	}
+
+}
