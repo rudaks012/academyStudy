711f1d4fcdf25bf95945d830f421264727d46226
man <rudaks012@naver.com>
2022-01-15 21:37:25 +0900

    경만파일

--- Movie/src/main/java/co/yedam/Movie/man/service/MoviePostMapper.java
+++ Movie/src/main/java/co/yedam/Movie/man/service/MoviePostMapper.java
@@ -2,7 +2,8 @@ package co.yedam.Movie.man.service;
 
 import java.util.List;
 
-import co.yedam.Movie.movieservice.MovieVO;
+import co.yedam.Movie.comm.MovieVO;
+
 
 public interface MoviePostMapper {
 	List<MovieVO> moviePostSelectList();	//영화 포스트 최근 3개 조회
--- Movie/src/main/java/co/yedam/Movie/man/serviceImpl/MoviePostServiceImpl.java
+++ Movie/src/main/java/co/yedam/Movie/man/serviceImpl/MoviePostServiceImpl.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import co.yedam.Movie.comm.MovieVO;
 import co.yedam.Movie.man.service.MoviePostMapper;
 import co.yedam.Movie.man.service.MoviePostService;
-import co.yedam.Movie.movieservice.MovieVO;
 
 @Repository("moviePostDao")
 public class MoviePostServiceImpl implements MoviePostService {
