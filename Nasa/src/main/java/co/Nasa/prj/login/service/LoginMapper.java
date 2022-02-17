03b0e81d5efe1f18c52ef5108e0a92078cf75fc8
rudaks012 <rudaks012@naver.com>
2022-02-17 17:39:10 +0900

    0217 login

--- Nasa/src/main/java/co/Nasa/prj/login/service/LoginMapper.java
+++ Nasa/src/main/java/co/Nasa/prj/login/service/LoginMapper.java
@@ -3,13 +3,7 @@ package co.Nasa.prj.login.service;
 import java.util.List;
 
 public interface LoginMapper {
-<<<<<<< HEAD
 	public List<LoginVO> selectListbuyer();
 	LoginVO selectbuyer(LoginVO vo);
 	LoginVO selectbuyerId(LoginVO vo);
-=======
-	public List<HostVO> selectListHost();
-	HostVO selectHost(HostVO vo);
-	HostVO selectHostId(HostVO vo);
->>>>>>> ded89f42a60daea296ac52ec8f876bfed2d3125f
 }
