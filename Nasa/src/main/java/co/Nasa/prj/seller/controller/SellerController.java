06caa4811cef89c4a340df6909db08767f86afd8
JungaWi <wja36750@gmail.com>
2022-02-17 17:31:09 +0900

    레이아웃 수정

--- Nasa/src/main/java/co/Nasa/prj/seller/controller/SellerController.java
+++ Nasa/src/main/java/co/Nasa/prj/seller/controller/SellerController.java
@@ -5,8 +5,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SellerController {
-	@RequestMapping("/go_seller.do")
-	public String go_seller() {
-		return "seller/sellerHome";
-	}
+	
 }
