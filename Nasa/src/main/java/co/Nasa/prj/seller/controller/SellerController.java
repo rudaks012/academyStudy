001b5c67dfdb18e30a39337f63dbc33a6cf8c92b
JungaWi <wja36750@gmail.com>
2022-02-21 17:46:56 +0900

    0221 학원 위정아

--- Nasa/src/main/java/co/Nasa/prj/seller/controller/SellerController.java
+++ Nasa/src/main/java/co/Nasa/prj/seller/controller/SellerController.java
@@ -59,6 +59,13 @@ public class SellerController {
 		return "seller/sellerPersonjoin";
 	}
 	
+	// 기업 판매자 회원가입
+	@RequestMapping("/sellerCompanyjoin.do")
+	public String sellerCompanyjoin() {
+		return "seller/sellerCompanyjoin";
+	}
+	
+	
 	@RequestMapping("/sellerPayment.do")
 	public String sellerPayment() {
 		return "seller/sellerPayment";
