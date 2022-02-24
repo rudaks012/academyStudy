743fe153b3e05ce0607a07ad51d9180b8f19f8c7
rudaks012 <rudaks012@naver.com>
2022-02-24 18:14:34 +0900

    check

--- Nasa/src/main/java/co/Nasa/prj/service/serviceImpl/ServiceServiceImpl.java
+++ Nasa/src/main/java/co/Nasa/prj/service/serviceImpl/ServiceServiceImpl.java
@@ -1,7 +1,14 @@
 package co.Nasa.prj.service.serviceImpl;
 
+import co.Nasa.prj.comm.VO.ServiceVO;
 import co.Nasa.prj.service.service.ServiceService;
 
 public class ServiceServiceImpl implements ServiceService {
+
+	@Override
+	public int serviceInsert(ServiceVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
