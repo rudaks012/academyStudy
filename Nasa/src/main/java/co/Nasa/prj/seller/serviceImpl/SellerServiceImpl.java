19749da67ae77c306ef508347eebd398fc6464c4
JungaWi <wja36750@gmail.com>
2022-02-25 12:19:54 +0900

    0225 저앙

--- Nasa/src/main/java/co/Nasa/prj/seller/serviceImpl/SellerServiceImpl.java
+++ Nasa/src/main/java/co/Nasa/prj/seller/serviceImpl/SellerServiceImpl.java
@@ -1,7 +1,13 @@
 package co.Nasa.prj.seller.serviceImpl;
 
+import co.Nasa.prj.comm.VO.SellerVO;
 import co.Nasa.prj.seller.service.SellerService;
 
 public class SellerServiceImpl implements SellerService {
+
+	@Override
+	public int SellerNicknameCheck(SellerVO vo) {
+		return 0;
+	}
 
 }
