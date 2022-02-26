b07c0bf0dff2e9a07f33d5a2ae3c60aa74ea4960
JMGyu <wjdals9577@naver.com>
2022-02-26 19:40:56 +0900

    review&review_comment view, vo, controller, service, mapper, map...

--- Nasa/src/main/java/co/Nasa/prj/review/serviceImpl/ReviewServiceImpl.java
+++ Nasa/src/main/java/co/Nasa/prj/review/serviceImpl/ReviewServiceImpl.java
@@ -1,7 +1,24 @@
 package co.Nasa.prj.review.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.Nasa.prj.comm.VO.ReviewVO;
+import co.Nasa.prj.review.service.ReviewMapper;
 import co.Nasa.prj.review.service.ReviewService;
 
 public class ReviewServiceImpl implements ReviewService {
+	@Autowired ReviewMapper map;
+
+	@Override
+	public List<ReviewVO> buyerSelectReviewList(ReviewVO vo) {
+		return map.buyerSelectReviewList(vo);
+	}
+
+	@Override
+	public int deleteReview(ReviewVO vo) {
+		return map.deleteReview(vo);
+	}
 
 }
--- Nasa/src/main/java/co/Nasa/prj/review_comment/service/Review_CommentService.java
+++ Nasa/src/main/java/co/Nasa/prj/review_comment/service/Review_CommentService.java
@@ -1,5 +1,10 @@
 package co.Nasa.prj.review_comment.service;
 
-public interface Review_CommentService {
+import java.util.List;
 
+import co.Nasa.prj.comm.VO.Review_CommentVO;
+
+public interface Review_CommentService {
+	// 리뷰 댓글 전체 조회
+	public List<Review_CommentVO> selectReview_CommentList();
 }
