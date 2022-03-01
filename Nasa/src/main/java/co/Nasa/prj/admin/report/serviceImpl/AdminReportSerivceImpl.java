879f9801b932ed38b61d06fcc54db09c3d0f02e7
je9996 <je9996@naver.com>
2022-03-02 01:07:56 +0900

    신고관리

--- Nasa/src/main/java/co/Nasa/prj/admin/report/serviceImpl/AdminReportSerivceImpl.java
+++ Nasa/src/main/java/co/Nasa/prj/admin/report/serviceImpl/AdminReportSerivceImpl.java
@@ -1,10 +1,14 @@
 package co.Nasa.prj.admin.report.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import co.Nasa.prj.admin.report.service.AdminReportMapper;
 import co.Nasa.prj.admin.report.service.AdminReportService;
+import co.Nasa.prj.admin.service.Criteria;
+import co.Nasa.prj.comm.VO.ReportVO;
 
 @Repository("reportDao")
 public class AdminReportSerivceImpl implements AdminReportService {
@@ -16,5 +20,11 @@ public class AdminReportSerivceImpl implements AdminReportService {
 	public int todayReport() {
 		// TODO Auto-generated method stub
 		return map.todayReport();
+	}
+
+	@Override
+	public List<ReportVO> getListWithPaging(Criteria cri) {
+		// TODO Auto-generated method stub
+		return map.getListWithPaging(cri);
 	}
 }
